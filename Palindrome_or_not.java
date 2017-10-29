ac1a715abe278f8100cdfcfcb3ae623e7965bfd4
DeeJay-NuKe <ask222g@gmail.com>
2017-10-29 21:21:29 +0530

    Code cleanup

--- Palindrome_or_not.java
+++ Palindrome_or_not.java
@@ -1,38 +1,39 @@
+package javacodes;
+
 /*
 Write a program to check the given number is palindrome or not?
 Descritpion : A Palindrome number is a number that remains the same when its digits are reversed. Like 16461, for example: we take 121 and reverse it, after revers it is same as original number
-*/
-
+ */
 import java.util.Scanner;
 
-class P{
+class P {
 
-public static void main(String args[])
-{
-int n,x,i,number;
+    public static void main(String args[]) {
+        int n, x, i, number;
 
-boolean f=true;
+        boolean f = true;
 
-Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-number = sc.nextInt();
+        number = sc.nextInt();
 
-String str = Integer.toString(number);
+        String str = Integer.toString(number);
 
-String strArr[] = str.split("");
+        String strArr[] = str.split("");
 
-x = str.length();
-n=x/2;
-for(i=0;i<n;i++)
-{
-if(strArr[i].compareTo(strArr[--x])!=0)
-{f=false;break;}
+        x = str.length();
+        n = x / 2;
+        for (i = 0; i < n; i++) {
+            if (strArr[i].compareTo(strArr[--x]) != 0) {
+                f = false;
+                break;
+            }
+        }
+
+        if (f == true) {
+            System.out.print(str + " is palindrome ");
+        } else {
+            System.out.print(str + " is not a palindrome");
+        }
+    }
 }
-
-if(f==true)
-System.out.print(str +" is palindrome ");
-
-else
-System.out.print( str+" is not a palindrome");
-}
-} 
--- ProduConsum.java
+++ ProduConsum.java
@@ -1,119 +1,109 @@
-
 package javacodes;
 
-import java.util.*;
+public class ProduConsum {
 
-public class ProduConsum{
-    
-    public static void main(String []args)throws InterruptedException
-            
-    {
-        Queue q=new Queue();
-        
-        Producer p=new Producer(q);
-        
-        Consumer c=new Consumer(q);
-        
-        Thread pt=new Thread(p);
-        
-        Thread ct=new Thread(c);
-        
+    public static void main(String[] args) throws InterruptedException {
+        Queue q = new Queue();
+
+        Producer p = new Producer(q);
+
+        Consumer c = new Consumer(q);
+
+        Thread pt = new Thread(p);
+
+        Thread ct = new Thread(c);
+
         pt.start();
-        
+
         ct.start();
-        
+
         pt.join();
-        
+
         ct.join();
-        
+
         System.out.println("Existing main Thread");
     }
 }
-class Producer implements Runnable
-{
+
+class Producer implements Runnable {
+
     Queue queue;
-    
-    Producer(Queue queue)
-    {
-        this.queue=queue;
-        
-        
+
+    Producer(Queue queue) {
+        this.queue = queue;
+
     }
-    public void run()
-    {
-        int i=0;
-        
-        while(i<=20)
-        {
+
+    public void run() {
+        int i = 0;
+
+        while (i <= 20) {
             queue.set(i);
-            
+
             i++;
         }
         System.out.println("Existing Producer Thread");
-        
-        queue.producerAlive=false;
+
+        queue.producerAlive = false;
     }
 }
-class Consumer implements Runnable
-{
+
+class Consumer implements Runnable {
+
     Queue queue;
-    
-    Consumer(Queue queue)
-    {
-        this.queue=queue;
+
+    Consumer(Queue queue) {
+        this.queue = queue;
     }
-    public void run()
-    {
-        while(queue.producerAlive)
-        {
+
+    public void run() {
+        while (queue.producerAlive) {
             queue.get();
         }
         System.out.println("Existing Consumer Thread");
     }
 }
-class Queue
-{
-    int n=0;
-    
-    boolean valueset=false;
-    
-    boolean producerAlive=true;
-    
-    synchronized void get()
-    {
-        if(!valueset)
-        {
+
+class Queue {
+
+    int n = 0;
+
+    boolean valueset = false;
+
+    boolean producerAlive = true;
+
+    synchronized void get() {
+        if (!valueset) {
             try {
                 wait();
-                
+
             } catch (InterruptedException e) {
-                
+
                 System.out.println("Error");
             }
         }
-        valueset=false;
-        
-        System.out.println("Got"+" "+this.n);
-        
+        valueset = false;
+
+        System.out.println("Got" + " " + this.n);
+
         notify();
     }
-    synchronized void set(int n)
-    {
-        if(valueset)
-        {
+
+    synchronized void set(int n) {
+        if (valueset) {
             try {
-                
+
                 wait();
-                
+
             } catch (InterruptedException e) {
-                
+
                 System.out.println("Value set");
             }
         }
-        this.n=n;
-        
-        valueset=true;
-        
+        this.n = n;
+
+        valueset = true;
+
         notify();
     }
 }
